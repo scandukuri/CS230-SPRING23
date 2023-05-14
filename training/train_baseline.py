59ef0be32d951c066ff34d8f79bac67a934056cb
Chinmaya Andukuri <andukuri@stanford.edu>
2023-05-13 20:18:08 -0700

    Adding baseline training script.

--- training/train_baseline.py
+++ training/train_baseline.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from datasets import Dataset
 import torch
 from transformers import (
     GPT2LMHeadModel,
@@ -9,34 +8,64 @@ from transformers import (
 )
 from transformers import Trainer, TrainingArguments
 from torch.optim import AdamW
+import re
 import os
 
-model = GPT2LMHeadModel.from_pretrained("distilgpt2")
 
-# Load and preprocess data
-os.chdir("/Users/scandukuri/CS230-SPRING23/")
-csv_file = "data/processed/train.csv"
-data = pd.read_csv(csv_file)
-data["formatted_text"] = (
-    "Genre: " + data["tag"] + " Title: " + data["title"] + " Lyrics: " + data["lyrics"]
-)
-text_data = data["formatted_text"].tolist()
-
-
-def save_text_data(text_data, filename):
-    with open(filename, "w", encoding="utf-8") as f:
-        for line in text_data:
-            f.write(f"{line}\n")
-
-
-save_text_data(text_data, "data/processed/train_lyrics_data.txt")
-
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+device = torch.device("cuda")
+model = GPT2LMHeadModel.from_pretrained("distilgpt2").to(device)
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2").to(device)
+tokenizer.bos_token = "<|startoftext|>"
+tokenizer.eos_token = "<|endoftext|>"
 tokenizer.pad_token = tokenizer.eos_token
 
 
 def tokenize_function(examples):
     return tokenizer(examples["text"], truncation=True, max_length=512)
+
+
+# Load and preprocess data
+# os.chdir("/Users/scandukuri/CS230-SPRING23/")
+csv_file = "data/processed/train.csv"
+data = pd.read_csv(csv_file)
+data["formatted_text"] = (
+    f"{tokenizer.bos_token} Genre: "
+    + data["tag"]
+    + " Title: "
+    + data["title"]
+    + " Lyrics: "
+    + data["lyrics"]
+    + f" {tokenizer.eos_token}"
+)
+
+
+pattern = r"\[.*?\]"
+text_data = data["formatted_text"].tolist()
+
+# remove tags like [Chorus: ] etc
+
+
+def save_text_data(text_data, filename):
+    with open(filename, "w", encoding="utf-8") as f:
+        for line in text_data:
+            if isinstance(line, str):
+                line = re.sub(r"\[.*?\]", "", line)
+                line = re.sub(r"\(.*?Chorus.*?\)", "", line)
+                line = re.sub(r"\(.*?Verse.*?\)", "", line)
+                line = re.sub(r"\(.*?Hook.*?\)", "", line)
+                line = re.sub(r"\(.*?Intro.*?\)", "", line)
+                line = re.sub(r"\(.*?Outro.*?\)", "", line)
+                line = re.sub(r"\(.*?Pre-.*?\)", "", line)
+                line = re.sub(r"\(.*?chorus.*?\)", "", line)
+                line = re.sub(r"\(.*?verse.*?\)", "", line)
+                line = re.sub(r"\(.*?hook.*?\)", "", line)
+                line = re.sub(r"\(.*?intro.*?\)", "", line)
+                line = re.sub(r"\(.*?outro.*?\)", "", line)
+                line = re.sub(r"\(.*?pre-.*?\)", "", line)
+                f.write(f"{line}\n")
+
+
+save_text_data(text_data, "data/processed/train_lyrics_data.txt")
 
 
 train_dataset = TextDataset(
@@ -45,6 +74,7 @@ train_dataset = TextDataset(
     block_size=128,
 )
 
+
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer,
     mlm=False,
@@ -52,12 +82,11 @@ data_collator = DataCollatorForLanguageModeling(
 
 
 training_args = TrainingArguments(
-    output_dir="models/baseline",
+    output_dir="models/baseline/train",
     overwrite_output_dir=True,
     num_train_epochs=1,
     per_device_train_batch_size=4,
-    save_steps=100_000,
-    save_total_limit=2,
+    save_steps=10_000,
 )
 
 
@@ -83,5 +112,5 @@ trainer = CustomTrainer(
 )
 
 trainer.train()
-trainer.save_model("models/baseline/final")
-tokenizer.save_pretrained("./output_dir")
+trainer.save_model("models/baseline/train")
+tokenizer.save_pretrained("models/baseline/train")
