f4780bd9601e2b8b4ecc9fff324e40e9bc24dc49
Chinmaya Andukuri <andukuri@stanford.edu>
2023-05-19 01:19:40 -0700

    Final edited files pre-milestone.

--- training/train_baseline.py
+++ training/train_baseline.py
@@ -6,51 +6,91 @@ from transformers import (
     TextDataset,
     DataCollatorForLanguageModeling,
 )
-from transformers import Trainer, TrainingArguments
+from transformers import Trainer, TrainingArguments, logging
 from torch.optim import AdamW
 import re
 import os
+import datasets
+
+dataset = datasets.load_dataset("text", data_dir="trainingdataset")["train"]
+max_seq_length = 128
+num_proc = 8
+
+logging.set_verbosity_info()
+logger = logging.get_logger("transformers")
+logger.info("INFO")
+logger.warning("WARN")
 
 
 device = torch.device("cuda")
 model = GPT2LMHeadModel.from_pretrained("distilgpt2").to(device)
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2").to(device)
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 tokenizer.bos_token = "<|startoftext|>"
 tokenizer.eos_token = "<|endoftext|>"
 tokenizer.pad_token = tokenizer.eos_token
 
 
 def tokenize_function(examples):
-    return tokenizer(examples["text"], truncation=True, max_length=512)
+    # Remove empty lines
+    examples["text"] = [
+        line for line in examples["text"] if len(line) > 0 and not line.isspace()
+    ]
+    return tokenizer(
+        examples["text"],
+        truncation=True,
+        max_length=max_seq_length,
+    )
 
 
-train_dataset = TextDataset(
-    tokenizer=tokenizer,
-    file_path="data/processed/train_lyrics_data.txt",
-    block_size=128,
+tokenized_dataset = dataset.map(
+    tokenize_function,
+    batched=True,
+    num_proc=num_proc,
+    remove_columns=["text"],
 )
 
+
+def group_texts(examples):
+    # Concatenate all texts.
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, we could add padding if the model supported it instead of this drop,
+    # you can customize this part to your needs.
+    total_length = (total_length // max_seq_length) * max_seq_length
+    # Split by chunks of max_len.
+    result = {
+        k: [t[i : i + max_seq_length] for i in range(0, total_length, max_seq_length)]
+        for k, t in concatenated_examples.items()
+    }
+    return result
+
+
+train_dataset = tokenized_dataset.map(
+    group_texts,
+    batched=True,
+    num_proc=num_proc,
+)
+
+print("Loaded training file into text dataset successfully.")
 
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer,
     mlm=False,
 )
 
-
 training_args = TrainingArguments(
-    output_dir="models/baseline/train",
+    output_dir="models",
     overwrite_output_dir=True,
     num_train_epochs=1,
     per_device_train_batch_size=4,
-    save_steps=10_000,
-    save_total_limit=2,
+    save_steps=500000,
 )
 
 
 class CustomTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         labels = inputs.pop("labels")
-        outputs = model(**inputs, labels=labels)
+        outputs = model(inputs["input_ids"], labels=labels)
         logits = outputs.logits
         loss_fct = torch.nn.CrossEntropyLoss()
         labels_shifted = labels[..., 1:].contiguous()
@@ -69,5 +109,5 @@ trainer = CustomTrainer(
 )
 
 trainer.train()
-trainer.save_model("models/baseline/train")
-tokenizer.save_pretrained("models/baseline/train")
+trainer.save_model("models")
+tokenizer.save_pretrained("models")
